58f8d95cd369e90ea1e896d332d48e0fd8048728
Emberclap <133802297+Emberclap@users.noreply.github.com>
2023-10-09 17:03:29 +0300

    Add project files.

---
+++ GenericScale/EqualityScale.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        private T left;
+        private T right;
+        public EqualityScale(T left, T right)
+        {
+            Left = left;
+            Right = right;
+        }
+
+        public T Left { get; set; }
+        public T Right { get; set; }
+        public bool AreEqual()
+        {
+            if (Left.Equals(Right))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
